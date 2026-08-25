40270b69a2f03cca83371e0db2e6ea3c4011b700
Ruslan <jer_X_2014_projekt@mail.ru>
2026-08-25 21:42:15 +0300

    Container and h1 title aded

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
 import "@mantine/core/styles.css";
-
-import { MantineProvider } from "@mantine/core";
+import ky from "ky";
+import { Container, Group, MantineProvider, Title } from "@mantine/core";
+import { useEffect } from "react";
 
 function App() {
-  return <MantineProvider></MantineProvider>;
+  useEffect(() => {
+    async function getLaunches() {
+      const launches = await ky(
+        "https://kata-spacex.onrender.com/api/launches ",
+      ).json();
+    }
+
+    getLaunches();
+  }, []);
+
+  return (
+    <MantineProvider>
+      <Container size={"sm"}>
+        <Title order={1} c={"black"} ta={"center"}>
+          SpaceX Launches 2020
+        </Title>
+        <Group component={"ul"}></Group>
+      </Container>
+    </MantineProvider>
+  );
 }
 
 export default App;
